e807de917a775dd9a05025c6f8fbd2828fbc19d3
Paweł <pl85@o2.pl>
2018-06-30 14:32:28 +0200

    commit for conflict

--- src/main/java/ExampleClass3.java
+++ src/main/java/ExampleClass3.java
@@ -1,4 +1,14 @@
 public class ExampleClass3 {
 //jakis tam komentarz
+public static void main(String[] args) {
+    System.out.println("Hello");
+
+    int x = 0;
+    int y= x+5;
+    int z = 5*y;
+
+    int suma = y*z;
+    System.out.println(suma);
+}
 
 }
